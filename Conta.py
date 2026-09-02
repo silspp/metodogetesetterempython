c5de76c26c75d0fb064e0e81c42660e2d14d459f
silvio <150728497+silspp@users.noreply.github.com>
2026-09-02 22:58:32 +0000

    mais alt

--- Conta.py
+++ Conta.py
@@ -19,3 +19,12 @@ class Conta:
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+# from conta import Conta
+#>>> conta = Conta(123, "Nico", 55.5, 1000.0)
+#Construindo objeto ... <conta.Conta object at 0x7fc4ed132048>
+#>>> conta2 = Conta(321, "Marcos", 100.0, 1000.0)
+#Construindo objeto ... <conta.Conta object at 0x7fc4ed1324a8>
+#>>> conta.titular
+#'Nico'
+#>>> conta2.titular
+#'Marcos'
