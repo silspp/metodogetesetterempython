25892fbe7e76ec2762c41fe33ac4255236161945
silvio <150728497+silspp@users.noreply.github.com>
2026-09-02 23:02:45 +0000

    implementacao

--- Conta.py
+++ Conta.py
@@ -19,6 +19,44 @@ class Conta:
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+        class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
 # from conta import Conta
 #>>> conta = Conta(123, "Nico", 55.5, 1000.0)
 #Construindo objeto ... <conta.Conta object at 0x7fc4ed132048>
